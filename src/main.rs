1cb2b996c7e4aba08c23e5029e90cbe15c6c048f
Ian Ndeda <ian.ndeda@gmail.com>
2024-05-13 11:57:15 +0300

    Added cli prompt for input.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-// Prints 'Hello, world!' to the terminal
+// Prints 'Hello, {input from the cli}!' to the terminal
 fn main() {
-    println!("Hello, world!");
+    let mut hold = String::new();
+    println!("Please enter who you wanna holla at: ");
+    std::io::stdin().read_line(&mut hold).unwrap();
+    
+    let name = hold.trim();
+
+    println!("Hello, {}!", name);
 }
